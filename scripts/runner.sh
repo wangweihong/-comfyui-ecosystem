94ae6db2785b88cb858262e97cbed3cf4edb5309
wwhvw <wangweihong>
2026-02-03 00:37:22 +0800

    update

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-sudo docker run -it  \
+
+docker run -it  \
   --name comfyui \
   --gpus 1 \
   -p 8188:8188 \
   -v "$(pwd)"/storage:/root \
-  -v "$(pwd)"/storage-models/models:/root/ComfyUI/models \
-  -v "$(pwd)"/storage-models/hf-hub:/root/.cache/huggingface/hub \
-  -v "$(pwd)"/storage-models/torch-hub:/root/.cache/torch/hub \
+  -v  /media/wwhvw/A63032EE3032C5591/comfyui-docker/storage-models/models:/root/ComfyUI/models \
+  -v /media/wwhvw/A63032EE3032C5591/comfyui-docker/storage-models/hf-hub:/root/.cache/huggingface/hub \
+  -v /media/wwhvw/A63032EE3032C5591/comfyui-docker/storage-models/torch-hub:/root/.cache/torch/hub \
   -v "$(pwd)"/storage-user/input:/root/ComfyUI/input \
   -v "$(pwd)"/storage-user/output:/root/ComfyUI/output \
-  -v "$(pwd)"/storage-user/workflows:/root/ComfyUI/user/default/workflows \
-  -v "$(pwd)"/storage-user/subgraphs:/root/ComfyUI/user/default/subgraphs \
+  -v /home/wwhvw/codespace/comfyui-ecosystem/workflows:/root/ComfyUI/user/default/workflows \
+  -v /home/wwhvw/codespace/comfyui-ecosystem/subgraphs:/root/ComfyUI/user/default/subgraphs \
   --entrypoint bash \
   -e CLI_ARGS="--disable-xformers" \
+  -e HF_TOKEN="https://hf-mirror.com" \
   registry.cn-hangzhou.aliyuncs.com/eazycloud/comfyui-boot:cu128-megapak
+
+
+# cli-args参数： --disable-smart-memory： 这个参数可以让comfyui将显存的数据大量搬运到内存。
+#               --disable-xformers: 不设置某些模型会出现花图
